4d21d1d955c292e5e63bd962e1982dd95c4a0294
rcayabyab <cayabyab.ryan@gmail.com>
2016-04-26 11:54:24 -0400

    generic slide styles

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -16,11 +16,11 @@ $(document).ready(function(){
 	// HEADER BACKGROUND CHANGE ON SCROLL
 	var header = $('header');
 	var full = $('section.hero h4');
-	var range = 200; // USE IF YOU WANT A RANGE
+	var range = 250; // USE IF YOU WANT A RANGE
 	$(window).on('scroll', function () {
     var scrollTop = $(this).scrollTop();
     var offset = full.offset().top;
-    var opac = scrollTop / offset;
+    var opac = scrollTop / range;
     header.css('background-color', 'rgba(9, 59, 82,' + opac + ')');
   	// console.log(scrollTop, offset, opac);
     if ( opac > '1' ) {
