78d065ac388f13ebe8dea472956a2f5ea6edeafc
rcayabyab <cayabyab.ryan@gmail.com>
2016-04-30 18:26:06 -0400

    fixed header fade js

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
 
 	// HEADER BACKGROUND CHANGE ON SCROLL
 	var header = $('body > header');
-	var full = $('section.hero h4');
+	// var full = $('section.hero h4');
 	var range = 250; // USE IF YOU WANT A RANGE
 	$(window).on('scroll', function () {
     var scrollTop = $(this).scrollTop();
-    var offset = full.offset().top;
+    // var offset = full.offset().top;
     var opac = scrollTop / range;
     header.css('background-color', 'rgba(9, 59, 82,' + opac + ')');
-  	// console.log(scrollTop, offset, opac);
+  	console.log(scrollTop, opac);
     if ( opac > '1' ) {
       header.css('background-color', 'rgba(9, 59, 82, 1)');
     } else if ( opac < '0' ) {
